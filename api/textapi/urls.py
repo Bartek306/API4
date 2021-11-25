b12650f9dcffbe9031c2b8b10ed5e9e20baab995
Bartek306 <shwdp13@wp.pl>
2021-11-25 13:33:10 +0100

    Fix urls

--- api/textapi/urls.py
+++ api/textapi/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import archive_convert
+from .views import archive_convert, convert
 
 urlpatterns = [
-    path('archive_convert', archive_convert)
-
+    path('archive_convert', archive_convert),
+    path('convert', convert)
 ]
